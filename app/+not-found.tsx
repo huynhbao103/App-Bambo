6bad94fd16d1e3a615167b235ce914877c2da18e
Bao <huynhbao103@gmail.com>
2025-01-12 18:54:49 +0700

    CICD AES #4

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -6,7 +6,7 @@ const NotFound: React.FC = () => {
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
-      <Link to="/">Go to Home</Link>
+     
     </div>
   );
 };
